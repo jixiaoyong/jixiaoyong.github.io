c4adcbc756a8ac8dcf9ff1829b505e2fe2613eed
jixiaoyong <jixiaoyong1995@gmail.com>
2023-10-11 17:55:09 +0800

    [action]更新waline服务器地址 by JI,XIAOYONG
    auto update by github action, trigged by blog_source_code@f90027e on 2023-10-11 04:53:08

---
+++ blog/assets/pageview-16b44f50.js
@@ -0,0 +1,1 @@
+import{a9 as e}from"./app-ec32f948.js";const a=()=>e({serverURL:"https://blog-comment-walinejs.vercel.app"});export{a as updatePageview};
--- blog/assets/pageview-e5a29ce0.js
+++
@@ -1,1 +0,0 @@
-import{a9 as e}from"./app-e126c796.js";const a=()=>e({serverURL:"https://blog-comment-walinejs.vercel.app/"});export{a as updatePageview};
