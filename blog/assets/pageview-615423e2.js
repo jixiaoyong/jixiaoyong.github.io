36344e32b03f01eb49f740f4cf63e9cdb31bd770
jixiaoyong <jixiaoyong1995@gmail.com>
2024-01-02 21:28:45 +0800

    [action]Update index.scss by JI,XIAOYONG
    auto update by github action, trigged by blog_source_code@60d13e7 on 2024-01-02 21:26:40

---
+++ blog/assets/pageview-615423e2.js
@@ -0,0 +1,1 @@
+import{aa as e}from"./app-a0ec2c43.js";const a=()=>e({serverURL:"https://vercel.android666.cf"});export{a as updatePageview};
--- blog/assets/pageview-c02d8f3e.js
+++
@@ -1,1 +0,0 @@
-import{aa as e}from"./app-bf457b07.js";const a=()=>e({serverURL:"https://vercel.android666.cf"});export{a as updatePageview};
