74fdaee28b3f46ae27bb244b6bfd0caff1286e08
jixiaoyong <jixiaoyong1995@gmail.com>
2024-01-02 21:16:49 +0800

    [action]fix: blog修复NotByAi样式 by jixiaoyong
    auto update by github action, trigged by blog_source_code@b032e77 on 2024-01-02 21:14:28

--- blog/assets/pageview-36ae66f2.js
+++
@@ -1,1 +0,0 @@
-import{aa as e}from"./app-b6d82bb7.js";const a=()=>e({serverURL:"https://vercel.android666.cf"});export{a as updatePageview};
---
+++ blog/assets/pageview-c02d8f3e.js
@@ -0,0 +1,1 @@
+import{aa as e}from"./app-bf457b07.js";const a=()=>e({serverURL:"https://vercel.android666.cf"});export{a as updatePageview};
