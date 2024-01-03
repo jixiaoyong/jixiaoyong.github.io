b1848f009e8f963a76e06c4e6cfa09d5173bfc54
jixiaoyong <jixiaoyong1995@gmail.com>
2024-01-03 20:58:41 +0800

    [action]feat: 更新blog和imissyou的Waline版本到2.15.8 by jixiaoyong
    auto update by github action, trigged by blog_source_code@909fd5f on 2024-01-03 20:56:27

--- blog/assets/pageview-19134ba2.js
+++
@@ -1,1 +0,0 @@
-import{aa as e}from"./app-4152d5c1.js";const a=()=>e({serverURL:"https://vercel.android666.cf"});export{a as updatePageview};
---
+++ blog/assets/pageview-624f5786.js
@@ -0,0 +1,1 @@
+import{aa as e}from"./app-54b0e827.js";const a=()=>e({serverURL:"https://vercel.android666.cf"});export{a as updatePageview};
