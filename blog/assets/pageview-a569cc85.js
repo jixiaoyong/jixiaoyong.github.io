39ab3c161382c1aaf5d7f367ac22cfe0cf29caa5
jixiaoyong <jixiaoyong1995@gmail.com>
2024-05-02 21:21:50 +0800

    [action]Merge pull request #14 from jixiaoyong/dependabot/npm_and_yarn/blog/ejs-3.1.10 by JI,XIAOYONG
    auto update by github action, trigged by blog_source_code@4f34952 on 2024-05-02 21:19:47

--- blog/assets/pageview-a569cc85.js
+++
@@ -1,1 +0,0 @@
-import{aa as e}from"./app-b410525b.js";const a=()=>e({serverURL:"https://comment.apphub.prvcy.page"});export{a as updatePageview};
---
+++ blog/assets/pageview-ab2b5209.js
@@ -0,0 +1,1 @@
+import{aa as e}from"./app-9ad43e8a.js";const a=()=>e({serverURL:"https://comment.apphub.prvcy.page"});export{a as updatePageview};
