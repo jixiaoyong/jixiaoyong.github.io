28161ed16986f5a07c9d4d1f53f4ea6bcda1bafa
jixiaoyong <jixiaoyong1995@gmail.com>
2025-05-17 14:57:16 +0800

    chore: [action] feat(blog): 新增关于 Git Merge+Revert 与 Reset+Cherry-pick 的对比文章，分析两种方案的优缺点及推荐使用方式。  by JI,XIAOYONG
    Auto update by github action, trigged by blog_source_code@fe7205d on 2025-05-17 14:54:50

---
+++ blog/assets/classDiagram-MKYM2BOE-BobMTlfF.js
@@ -0,0 +1,1 @@
+import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-Dkai4xn3.js";import"./chunk-IO347I67-JakXlPJ7.js";import{m as o}from"./mermaid.esm.min-BCQ5bOIY.js";import"./app-CGvuCGfK.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
