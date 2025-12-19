ce8e8842e90733752791126d4790b187d7fbb9c8
jixiaoyong <jixiaoyong1995@gmail.com>
2025-12-19 23:24:59 +0800

    feat: [action] Implement new font loading strategy, add BackToTop and SocialLinks components, update favicons and avatars, and enhance copyright URL truncation.  by JI,XIAOYONG
    Auto update by github action, trigged by blog_source_code@4f8bd99 on 2025-12-19 23:22:34

---
+++ imissyou/assets/index_en-D-HTuB87.js
@@ -0,0 +1,15 @@
+const e=`---
+title: About
+date: 2016-06-06 6:06:06
+---
+
+
+
+Daily life is filled with various experiences, trivial matters, and thoughts. When I encounter them, I often feel moved and compelled to express myself. Most of the articles on this site are my observations and realizations of the world over these years.
+
+There are no masterpieces here that will go down in history, but they are indeed records hastily gathered during my journey through life. Occasionally, I revisit them for a bit of solace.
+
+![The Meaning of This Site](https://jixiaoyong.github.io/images/imissyou/about_me_001.png)
+
+<br/>
+`;export{e as default};
